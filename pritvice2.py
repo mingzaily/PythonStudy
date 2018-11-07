d65e47104a9769b2c3968343405cd0f8c11f558e
mingzaily <cst_zhang@163.com>
2018-11-07 19:07:29 +0800

    change style

--- pritvice2.py
+++ pritvice2.py
@@ -10,12 +10,10 @@ def rightRevt():
     can.move(rect,3,0)
 
 win=Tk()
-win.geometry('250x300')
+win.geometry('400x360')
 
 btn1=Button(win,text='向上',command=upRevt)
 btn1.pack(side='top')
-btn2=Button(win,text='向下',command=downRevt)
-btn2.pack()
 btn3=Button(win,text='向左',command=leftRevt)
 btn3.pack(side='left')
 btn4=Button(win,text='向右',command=rightRevt)
@@ -23,10 +21,11 @@ btn4.pack(side='right')
 
 can=Canvas(win,width=250,height=300)    #画布对象
 can.pack()
-
 x=100
 y=100
-
 rect=can.create_rectangle(x,y,x+30,y+30,fill='red')
+
+btn2=Button(win,text='向下',command=downRevt)
+btn2.pack()
 
 win.mainloop()
